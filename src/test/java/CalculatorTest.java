11a4d0ed8abbb300e197c09b5d009128a4179280
Mikhail Sushchikh <sumux200013@gmail.com>
2022-11-12 02:51:45 +0400

    add testSum function

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
+    @Test
+    public void testSum(){
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int a = 2;
+        int b = 2;
+        int expected = 4;
+
+        // WHEN
+        int actual = calculator.sum(a, b);
+
+        // THEN
+        Assert.assertEquals(expected, actual);
+    }
 }
