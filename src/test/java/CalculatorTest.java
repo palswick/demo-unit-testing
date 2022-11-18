2bd3b015c37983e162d424ae3232a3568bad424f
Mikhail Sushchikh <sumux200013@gmail.com>
2022-11-18 23:03:03 +0400

    add 4 unit tests for operations *-+/

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,4 +16,49 @@ public class CalculatorTest {
         // THEN
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testMultiply(){
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int a = 3;
+        int b = 2;
+        int expected = 6;
+
+        // WHEN
+        int actual = calculator.multiply(a, b);
+
+        // THEN
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtract(){
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int a = 3;
+        int b = 2;
+        int expected = 1;
+
+        // WHEN
+        int actual = calculator.subtract(a, b);
+
+        // THEN
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide(){
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int a = 4;
+        int b = 2;
+        int expected = 2;
+
+        // WHEN
+        int actual = calculator.subtract(a, b);
+
+        // THEN
+        Assert.assertEquals(expected, actual);
+    }
 }
