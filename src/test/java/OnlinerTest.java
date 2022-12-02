ffafb18d9b6e3ca726fda9734dc3e3ea0df73b2f
Mikhail Sushchikh <sumux200013@gmail.com>
2022-12-02 21:58:50 +0400

    add testOpenBaraholkaPage

--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -17,16 +17,42 @@ public class OnlinerTest {
         WebDriver driver = new ChromeDriver();
         String baseUrl = "https://www.onliner.by/";
         String xpath = "//div[@class='footer-style__copy']";
+        String expected = "© 2001—2022 Onlíner";
 
         //WHEN
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.xpath(xpath));
-        Util.waiter();
+//        Util.waiter();
         String actual = element.getText();
 
         //THEN
-        System.out.println(actual);
+        Assert.assertEquals(expected, actual);
         driver.quit();
-        Assert.fail();
+    }
+
+    @Test
+    public void testOpenBaraholkaPage() {
+        //GIVEN
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "https://www.onliner.by/";
+        String xpathBaraholkaLink1 = "//*[@id=\"navigation-sidebar\"]/div[2]/a";
+        String xpathBaraholkaLink2 = "//*[@id=\"navigation-sidebar\"]/div[2]/div[2]/div[2]/ul[1]/li[7]/a";
+        String expected = "Барахолка";
+        String xpathBaraholkaTitle = "//*[@id=\"minWidth\"]/div/div[2]/div[2]/div/div[2]/div[2]/div[1]/h1";
+        driver.get(baseUrl);
+
+        //WHEN
+        WebElement elementBaraholkaLink1 = driver.findElement(By.xpath(xpathBaraholkaLink1));
+        elementBaraholkaLink1.click();
+        WebElement elementBaraholkaLink2 = driver.findElement(By.xpath(xpathBaraholkaLink2));
+        elementBaraholkaLink2.click();
+        WebElement elementBaraholkaTitle = driver.findElement(By.xpath(xpathBaraholkaTitle));
+        String actual = elementBaraholkaTitle.getText();
+
+
+        //THEN
+        Assert.assertEquals(expected, actual);
+        driver.quit();
     }
 }
